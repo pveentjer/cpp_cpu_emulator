e89126d252d170a314078d770519aab262a813c3
pveentjer <alarmnummer@gmail.com>
2024-04-26 16:54:38 +0300

    Cleanup

--- main.cpp
+++ main.cpp
@@ -4,24 +4,47 @@
 static const int REGISTER_COUNT = 32;
 static const int MEMORY_SIZE = 16;
 
-class CPU;
+static const int OPCODE_ADD = 1;
+static const int OPCODE_INC = 2;
+static const int OPCODE_LOAD = 3;
+static const int OPCODE_STORE = 4;
+static const int OPCODE_PRINT = 5;
+static const int OPCODE_HALT = 6;
 
-class Instruction {
-public:
-    virtual void execute(CPU *cpu) = 0;
+struct Instruction {
+    int opcode;
+    union {
+        struct {
+            int src1, src2, dst;
+        } add;
+        struct {
+            int src, dst;
+        } load;
+        struct {
+            int src, dst;
+        } store;
+        struct {
+            int src;
+        } print;
+        struct {
+            int src;
+        } inc;
+        struct {
+        } halt;
+    } code;
 };
 
 class CPU {
 
 public:
     int32_t ip;
-    std::vector<Instruction *> *program;
+    std::vector<Instruction> *program;
     std::vector<int> *registers;
     std::vector<int> *memory;
 
     CPU() {
         ip = 0;
-        program = new std::vector<Instruction *>();
+        program = new std::vector<Instruction>();
         registers = new std::vector<int>();
         for (int k = 0; k < REGISTER_COUNT; k++) {
             registers->push_back(0);
@@ -32,10 +55,50 @@ public:
         }
     }
 
-    void print_memory(){
+    void print_memory() {
         printf("------------------Memory----------------\n");
-        for(int k=0;k<memory->size();k++){
-            printf("%04d %04d\n",k, memory->at(k));
+        for (int k = 0; k < memory->size(); k++) {
+            printf("%04d %04d\n", k, memory->at(k));
+        }
+    }
+
+    void execute(Instruction *instr) {
+        switch (instr->opcode) {
+            case OPCODE_ADD: {
+                int a = registers->at(instr->code.add.src1);
+                int b = registers->at(instr->code.add.src2);
+                registers->at(instr->code.add.dst) = a + b;
+                ip++;
+                break;
+            }
+            case OPCODE_INC: {
+                int v = registers->at(instr->code.inc.src);
+                registers->at(instr->code.inc.src) = v + 1;
+                ip++;
+                break;
+            }
+            case OPCODE_LOAD: {
+                registers->at(instr->code.load.dst) = memory->at(instr->code.load.src);
+                ip++;
+                break;
+            }
+            case OPCODE_STORE: {
+                memory->at(instr->code.store.dst) = registers->at(instr->code.store.src);
+                ip++;
+                break;
+            }
+            case OPCODE_PRINT: {
+                int a = registers->at(instr->code.print.src);
+                printf("print %d\n", a);
+                ip++;
+                break;
+            }
+            case OPCODE_HALT: {
+                ip = -1;
+                break;
+            }
+            default:
+                throw std::runtime_error("foobar");
         }
     }
 
@@ -45,113 +108,31 @@ public:
 
     void tick() {
         printf("tick %d\n", ip);
-        Instruction *instruction = program->at(ip);
-        instruction->execute(this);
+        Instruction *instr = &program->at(ip);
+        execute(instr);
     }
 };
-
-class Add : public Instruction {
-
-public:
-
-    int src_1, src_2, dst;
-
-    Add(int src_1, int src_2, int dst) : src_1(src_1), src_2(src_2), dst(dst) {
-    }
-
-    void execute(CPU *cpu) override {
-        int a = cpu->registers->at(src_1);
-        int b = cpu->registers->at(src_2);
-        cpu->registers->at(dst) = a + b;
-        cpu->ip++;
-    }
-};
-
-class Load : public Instruction {
-
-public:
-
-    int src, dst;
-
-    Load(int src, int dst) : src(src), dst(dst) {
-    }
-
-    void execute(CPU *cpu) override {
-        cpu->registers->at(dst) = cpu->memory->at(src);
-        cpu->ip++;
-    }
-};
-
-
-class Inc: public Instruction {
-public:
-
-    int src, dst;
-
-    Inc(int src) : src(src) {
-    }
-
-    void execute(CPU *cpu) override {
-        int v = cpu->registers->at(src);
-        cpu->registers->at(src)=v+1;
-        cpu->ip++;
-    }
-};
-
-class Store : public Instruction {
-
-public:
-
-    int src, dst;
-
-    Store(int src, int dst) : src(src), dst(dst) {
-    }
-
-    void execute(CPU *cpu) override {
-        cpu->memory->at(src) = cpu->registers->at(dst);
-        cpu->ip++;
-    }
-};
-
-class Halt : public Instruction {
-    void execute(CPU *cpu) override {
-        cpu->ip = -1;
-        printf("halt\n");
-    }
-};
-
-class Print : public Instruction {
-public:
-    int src;
-
-    Print(int src) : src(src) {
-
-    }
-
-    void execute(CPU *cpu) override {
-        int a = cpu->registers->at(src);
-        printf("print %d\n", a);
-        cpu->ip++;
-    }
-};
-
 
 int main() {
     CPU *cpu = new CPU();
     cpu->memory->at(0) = 5;
     cpu->memory->at(1) = 10;
     cpu->memory->at(2) = 20;
+    cpu->program->push_back(Instruction());
+    cpu->program->back().opcode = OPCODE_LOAD;
+    cpu->program->back().code.load.src = 0;
+    cpu->program->back().code.load.dst = 0;
 
-    cpu->program->push_back(new Load(0,0));
-    cpu->program->push_back(new Inc(0));
-    cpu->program->push_back(new Load(1,1));
-    cpu->program->push_back(new Load(2,2));
+    cpu->program->push_back(Instruction());
+    cpu->program->back().opcode = OPCODE_INC;
+    cpu->program->back().code.inc.src = 0;
 
-    cpu->program->push_back(new Add(0, 1, 3));
-    cpu->program->push_back(new Add(2, 3, 4));
+    cpu->program->push_back(Instruction());
+    cpu->program->back().opcode = OPCODE_PRINT;
+    cpu->program->back().code.print.src = 0;
 
-    cpu->program->push_back(new Store(4,1));
-    cpu->program->push_back(new Halt());
+    cpu->program->push_back(Instruction());
+    cpu->program->back().opcode = OPCODE_HALT;
 
     while (!cpu->has_halted()) {
         cpu->tick();
